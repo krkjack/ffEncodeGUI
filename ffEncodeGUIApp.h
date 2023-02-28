37fb7901916d0056962684543617ccccdf51c011
krkjack <56771214+krkjack@users.noreply.github.com>
2023-02-28 14:08:31 +0100

    Uploadproject

---
+++ ffEncodeGUIApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      ffEncodeGUIApp.h
+ * Purpose:   Defines Application Class
+ * Author:    ABC ()
+ * Created:   2021-07-03
+ * Copyright: ABC ()
+ * License:
+ **************************************************************/
+
+#ifndef ffEncodeGUIAPP_H
+#define ffEncodeGUIAPP_H
+
+#include <wx/app.h>
+
+class ffEncodeGUIApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // ffEncodeGUIAPP_H
